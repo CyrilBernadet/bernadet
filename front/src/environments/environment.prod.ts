3f370cdb3889053e7921affa2331a854486e10c8
Cyril Bernadet <cyril.bernadet@gmail.com>
2022-11-01 13:15:25 +0100

    Change keycloak for HTTPS

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'http://kinikun.ddns.net:8081/api',
   keycloak: {
-    authUrl: 'http://kinikun.ddns.net:8080/auth',
+    authUrl: 'https://kinikun.ddns.net:8443/auth',
     realm: 'Bernadet',
     client: 'front'
   }
