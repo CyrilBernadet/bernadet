522790f64681ada32e8163094bd14e009321bbcc
Cyril Bernadet <cyril.bernadet@gmail.com>
2022-11-03 11:55:37 +0100

    Add hostnames

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://kinikun.ddns.net:8081/api',
+  apiUrl: 'http://backend:8081/api',
   keycloak: {
-    authUrl: 'https://kinikun.ddns.net:8443/auth',
+    authUrl: 'https://keycloak:8443/auth',
     realm: 'Bernadet',
     client: 'front'
   }
