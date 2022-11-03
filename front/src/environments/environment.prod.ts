407e1fb8e99c4108653376a75730ae61990a1d82
Cyril Bernadet <cyril.bernadet@gmail.com>
2022-11-03 18:26:33 +0100

    More HTTPS

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://bernadet.ddns.net:8081/api',
+  apiUrl: 'https://bernadet.ddns.net:8081/api',
   keycloak: {
     authUrl: 'https://bernadet.ddns.net:8443/auth',
     realm: 'Bernadet',
