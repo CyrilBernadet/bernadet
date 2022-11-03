470477fdc3b4a2f459b348d1acec2e150c859790
Cyril Bernadet <cyril.bernadet@gmail.com>
2022-11-03 12:08:02 +0100

    Front test

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://backend:8081/api',
+  apiUrl: 'http://bernadet.ddns.net:8081/api',
   keycloak: {
-    authUrl: 'https://keycloak:8443/auth',
+    authUrl: 'https://bernadet.ddns.net:8443/auth',
     realm: 'Bernadet',
     client: 'front'
   }
